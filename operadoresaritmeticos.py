db3b98451f60bc3fbb49041095526273e45e1701
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-30 12:01:53 -0300

    add commit

--- operadoresaritmeticos.py
+++ operadoresaritmeticos.py
@@ -1,5 +1,7 @@
 x = int(input('Digite um número:'))
 y = int(input('Digite outro número:'))
 z = x + y
+
 print('A soma dos números é:', z)
+
 print('Ou seja, a soma dos números', x, 'e', y, 'é:', z)
