b8b599279660e5ce3bd4fc964590683063d1b905
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-30 20:28:42 -0300

    add commit

--- codigo1.py
+++ codigo1.py
@@ -1,9 +1,18 @@
-faturamento = 1200
+faturamento = 1200  # número inteiro -> int
 custo = 270
 
 novas_vendas = 300
 faturamento = faturamento + novas_vendas
 
+taxa_imposto = 0.1  # número decimal -> float
+
+# texto -> string
+# verdadeiiro ou falso -> boolean
+
+imposto = taxa_imposto * faturamento
+
 print("Faturamento: R$", faturamento)
 print("Custo: R$", custo)
-print("Lucro: R$", faturamento - custo)
+print("Imposto sobre faturamento: R$", imposto)
+print("Lucro: R$", faturamento - custo - imposto)
+
