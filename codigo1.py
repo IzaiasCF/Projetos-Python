33e67a5e98943f314db1ebc8b489e50d9f138c12
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-30 20:18:02 -0300

    add commit

---
+++ codigo1.py
@@ -0,0 +1,9 @@
+faturamento = 1200
+custo = 270
+
+novas_vendas = 300
+faturamento = faturamento + novas_vendas
+
+print("Faturamento: R$", faturamento)
+print("Custo: R$", custo)
+print("Lucro: R$", faturamento - custo)
