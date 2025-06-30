f32ace32c43c43e2e52f2440a2ffa22ff1729c32
IzaiasCF <izaiascorreafilho@gmail.com>
2025-06-30 20:46:03 -0300

    add

---
+++ codigo2.py
@@ -0,0 +1,13 @@
+faturamento = 1000
+custo = 700
+
+novas_vendas = 300
+
+faturamento = faturamento + novas_vendas
+imposto = faturamento * 0.1
+lucro = faturamento - custo - imposto
+margem_lucro = lucro / faturamento
+
+print("Faturamento: R$", faturamento)
+print("Lucro: R$", lucro)
+print(margem_lucro)
