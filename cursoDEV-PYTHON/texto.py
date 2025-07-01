b2dea2f16e4827cbb4db1e50cad2140ff0b66701
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-01 13:34:01 -0300

    add commit

--- cursoDEV-PYTHON/texto.py
+++ cursoDEV-PYTHON/texto.py
@@ -4,6 +4,14 @@ email = "email_falso@gmail.com"
 
 lucro = faturamento - custo
 
-#print(f"Faturamento: R$ {faturamento}, Custo: R$ {custo}, Lucro: R$ {lucro}")
 print(f"Faturamento: R$ {faturamento}, Custo: R$ {custo}, Lucro: R$ {lucro}")
 print("E-mail:", email)
+
+email2 ="EMAIL_falso@gmail.com" 
+print(email2)  # mantendo upper case do exemplo acima
+print(email2.lower())   # com lower case
+print(email.upper())   # com upper case
+print(email.find("@"))  # posição do @ dentro de email, começando a contar do zero
+
+print(email[11])
+
