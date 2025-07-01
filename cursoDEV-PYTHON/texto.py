40acb35c9524bb3cb480ed3a23000ee8e36a1310
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-01 13:40:03 -0300

    add commit

--- cursoDEV-PYTHON/texto.py
+++ cursoDEV-PYTHON/texto.py
@@ -13,5 +13,5 @@ print(email2.lower())   # com lower case
 print(email.upper())   # com upper case
 print(email.find("@"))  # posição do @ dentro de email, começando a contar do zero
 
-print(email[11])  # mostra o que está na posição 11
+print(email[11])  # mostra o que está na posição [11]
 
