59ff7860344773eacc2d1206124fa2fc81e1e0c6
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-03 10:29:02 -0300

    add

--- cursoDEV-PYTHON/texto2.py
+++ cursoDEV-PYTHON/texto2.py
@@ -1,14 +1,14 @@
-# extrair servidor de email
+#  EXERCÍCIO
 nome = "Izaias Correa Filho"
 email = "izaiasfalso@gmail.com"
 
 # descobrir servidor de email.
 posicao = email.find("@")
 print(posicao)
-servidor = email[posicao:] # com arroba
-servidor = email[posicao:] # sem arroba
-print(servidor)
-
+servidor1 = email[posicao:] # com arroba
+servidor2 = email[posicao+1:] # sem arroba
+print(servidor1)
+print(servidor2)
 
 # extrair peimero nome de usuário.
 # contruir mensagem: Usuário Izaias Correa Filho cadastrado com sucessocom e o email.
