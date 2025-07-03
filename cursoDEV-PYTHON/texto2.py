0a05ef01574783ddbaa0de75cb4df63c5d9f00e6
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-03 10:10:41 -0300

    add commit

---
+++ cursoDEV-PYTHON/texto2.py
@@ -0,0 +1,14 @@
+# extrair servidor de email
+nome = "Izaias Correa Filho"
+email = "izaiasfalso@gmail.com"
+
+# descobrir servidor de email.
+posicao = email.find("@")
+print(posicao)
+servidor = email[posicao:]
+print(servidor)
+
+
+# extrair peimero nome de usuário.
+# contruir mensagem: Usuário Izaias Correa Filho cadastrado com sucessocom e o email.
+# contruir mensagem: Enviamos um link de confirmação para o email i****ogmail.com.
