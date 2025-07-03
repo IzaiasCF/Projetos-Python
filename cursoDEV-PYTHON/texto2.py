82d791ed858957a8bda216e91c3d2d40ff4e37a0
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-03 10:57:28 -0300

    add

--- cursoDEV-PYTHON/texto2.py
+++ cursoDEV-PYTHON/texto2.py
@@ -11,5 +11,16 @@ print(servidor1)
 print(servidor2)
 
 # extrair peimero nome de usuário.
+posicao = nome.find(" ")
+primeiro_nome = nome[:posicao]  # dois pontos na frente de "posicao" pega o priemrio ítem e exclui o restante
+print(primeiro_nome)
+
 # contruir mensagem: Usuário Izaias Correa Filho cadastrado com sucessocom e o email.
+mensagem = f"Usuário {primeiro_nome} cadastrado com sucesso, com o email: {email}"
+print(mensagem)
+
 # contruir mensagem: Enviamos um link de confirmação para o email i****ogmail.com.
+primeira_letra = email[0]
+print(primeira_letra)
+mensagem2 = f"Enviamos um link de confirmação para o email {primeira_letra}****{servidor2}"
+print(mensagem2)
