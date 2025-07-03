ec35bfb8e40b14b46f7c8d7547ba0e1d9c67cfd5
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-03 11:48:50 -0300

    add commit

--- cursoDEV-PYTHON/input2.py
+++ cursoDEV-PYTHON/input2.py
@@ -1,2 +1,11 @@
 # input para faturamento
 
+# definindo o float após o input
+vendas = input("Digite suas vendas do dia: ")
+vendas = float(vendas)
+
+# definindo o float noo input
+vendas = float(input("Digite suas vendas do dia: "))
+
+bonus = vendas * 0.01
+print(bonus)
