e21e8de29a8483f8bea6094744d5bd91179c614b
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-04 16:39:23 -0300

    addcommit

--- cursoDEV-PYTHON/listas.py
+++ cursoDEV-PYTHON/listas.py
@@ -21,27 +21,42 @@ vendas = [100, 50, 130, 80, 120, 200]
 # print(vendas[2:])  # exclui os primeiros ítens e mostra o restante da esquerda da lista
 # print(vendas[:2])  # pega os dois primeiros ítrens da lista e exclui o restante
 
-produtos = ["iPhone", "iPad", "AirPod", "MacBook"]
+produtos = ["iphone", "ipad", "airpod", "macbook"]
 #print("iPhone" in produtos)  # verifica se condição é verdadeira e rwsponde se TRUE ou FALSE, no caso, é TRUE
 #print("Vision Pro" in produtos)  # verifica se condição é verdadeira e rwsponde se TRUE ou FALSE, no caso, é FALSE
 
-produtos = ["iphone", "ipad", "AIRPODE"]
+produtos = ["iphone", "ipad", "airpod"]
 #produto_usuario = input("Digite o nome de um produto: ")
 #print(produto_usuario in produtos)
 precos = ["4000", "2000", "8000",]
 
 # Atualização de lisats
 
+# edita um ítem
 #print("iphone" in produtos)
 precos[0] = 4500
 precos[0] = precos[0] * 1.1
 novo_preco = precos[0] + 1.1
 #print(precos)
 
-produtos.append("macbook")
-precos.append(10000)
+produtos.append("macbook")  # APPEND - adiciona um ítem à lista.
+
+precos.append(10000)  # 
 precos[0] = novo_preco
 print(novo_preco)
 print(produtos)
 print(precos)
+
+# remover um ítem
+produtos.remove("macbook")
+precos.pop(-1)
+print(produtos)
+print(precos)
+
+# inserir um valor
+produtos.insert(1, "airpod")
+print(produtos)
+
+# contar valores
+print(produtos.count("airpod"))
 
