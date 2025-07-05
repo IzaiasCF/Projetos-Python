c9f77ab18ad4e3c7759970dad1459faa0714e395
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-05 15:01:45 -0300

    add

--- cursoDEV-PYTHON/condicao.py
+++ cursoDEV-PYTHON/condicao.py
@@ -18,15 +18,32 @@
 #     print(lucro)
 
 # condição com listas
-produtos = ["iphone", "ipad", "airpod"]
-novo_produto = input("Digite aqui o novo produto: ")
-novo_produto = novo_produto.lower()  # vai inserir na lista um novo produto, independente se digitarem em maiúsculo ou minúsculo
+# produtos = ["iphone", "ipad", "airpod"]
+# novo_produto = input("Digite aqui o novo produto: ")
+# novo_produto = novo_produto.lower()  # vai inserir na lista um novo produto, independente se digitarem em maiúsculo ou minúsculo
 
-if novo_produto in produtos:
-    print("Produto já cadastrado")
+# if novo_produto in produtos:
+#     print("Produto já cadastrado")
+# else:
+#     print("Produto cadastrado com sucesso!")
+#     produtos.append(novo_produto)  # acrescenta o novo produto à lista produtos
+
+# print(produtos)
+
+# Bóson Treinamentos
+## condicional simple
+n1 = n2 = 0.0
+media = 0.0
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+
+# calcular média aritmética
+media = (n1 + n2) / 2
+
+if (media >= 7):
+    print("Aluno aprovado!")
+    print(f"Sua média é:", media)
 else:
-    print("Produto cadastrado com sucesso!")
-    produtos.append(novo_produto)  # acrescenta o novo produto à lista produtos
-
-print(produtos)
-
+    print("Aluno reprovado!")
+    print(f"Sua média é:", media)
