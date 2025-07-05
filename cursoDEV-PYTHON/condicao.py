c38da9519dece5cf5e8e21abfa08d208375230b5
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-05 12:07:19 -0300

    add

--- cursoDEV-PYTHON/condicao.py
+++ cursoDEV-PYTHON/condicao.py
@@ -6,13 +6,25 @@
 # == = igual
 # != = diferente
 
-faturamento = 1000
-custo = 1800
-lucro = faturamento - custo
+# condição
+# faturamento = 1000
+# custo = 1800
+# lucro = faturamento - custo
 
-if lucro > 0:
-    print("Lucro R$", lucro)
+# if lucro > 0:
+#     print("Lucro R$", lucro)
+# else:
+#     print("Prejuízo!!!")
+#     print(lucro)
+
+# condição com listas
+produtos = ["iphone", "ipad", "airpod"]
+novo_produto = input("Digite aqui o novo produto: ")
+
+if novo_produto in produtos:
+    print("Produto já cadastrado")
 else:
-    print("Prejuízo!!!")
-    print(lucro)
+    print("Produto cadastrado com sucesso!")
+    produtos.append(novo_produto)  # acrescenta o novo produto à lista produtos
 
+print(produtos)
