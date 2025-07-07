88d37bf98d5d057f73a22fed82ee8dc52d556b16
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-07 17:12:30 -0300

    add file

---
+++ Boson-Treinamentos/print.py
@@ -0,0 +1,30 @@
+# Bóson Treinamentos
+
+## condicional simple
+# n1 = n2 = 0.0
+# media = 0.0
+
+# n1 = float(input("Digite a primeira nota: "))
+# n2 = float(input("Digite a segunda nota: "))
+
+# calcular média aritmética
+# media = (n1 + n2) / 2
+
+# if (media >= 7):
+#     print("Aluno aprovado!")
+#     print(f"Sua média é:", media)
+# else:
+#     print("Aluno reprovado!")
+#     print(f"Sua média é:", media)
+
+# Sintaxe
+#  print(objetos, argumentos)
+
+nome = input("Digite seu nome: ")
+print("Olá, " + nome + ". Bem vindo ao curso de Python!")
+
+# Ou: 
+nome = input("Digite seu nome: ")
+msg = "Olá, " + nome + ". Bem vindo ao curso de Python!"
+print(msg)
+
