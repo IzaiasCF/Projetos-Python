16ce9b19d5ae216ebe55d772570f8215d53d6c3b
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-07 16:55:24 -0300

    add commit

--- cursoDEV-PYTHON/condicao.py
+++ cursoDEV-PYTHON/condicao.py
@@ -30,21 +30,3 @@
 
 # print(produtos)
 
-# Bóson Treinamentos
-## condicional simple
-n1 = n2 = 0.0
-media = 0.0
-
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-
-# calcular média aritmética
-media = (n1 + n2) / 2
-
-if (media >= 7):
-    print("Aluno aprovado!")
-    print(f"Sua média é:", media)
-else:
-    print("Aluno reprovado!")
-    print(f"Sua média é:", media)
-
