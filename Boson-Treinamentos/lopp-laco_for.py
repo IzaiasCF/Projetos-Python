ed644bd65b38ef68aba56cc6aaf5e57062fd79b4
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-08 12:02:59 -0300

    add

--- Boson-Treinamentos/lopp-laco_for.py
+++ Boson-Treinamentos/lopp-laco_for.py
@@ -1,6 +1,14 @@
-lista = [2,6,9,4,0,12,3,7]
-palavra = "Boson"
+# lista = [2,6,9,4,0,12,3,7]
+# palavra = "Boson"
 
-for letra in palavra:
-  print(letra)
+# for letra in palavra:
+#   print(letra)
 
+# funçã0 range
+# for numero in range(1,11):
+#   print(numero)
+
+# OU:
+for numero in range(10):
+  print(numero)
+  
