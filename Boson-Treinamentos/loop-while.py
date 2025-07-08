b1cc07c842c44126044a4bf47b7d5eedb27ee28d
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-08 11:01:09 -0300

    add

--- Boson-Treinamentos/loop-while.py
+++ Boson-Treinamentos/loop-while.py
@@ -1,5 +1,20 @@
-num = 1
+# laço de repetição simples
+# num = 1
 
-while (num <= 10):
-  print(num)
-  
+# while (num <= 10):
+#   num += 1  # incremento
+  #print(num)
+#print("Laço encerrado!")
+
+# laço de repetição com if
+nome = None
+
+while True:
+  print("Digite seu nome, ou x para parar:")
+  nome = input()
+  if nome == "x" or nome == "X":
+    break  # fim do laço
+  print(f"Bem-vindo, {nome}")
+
+print("Até logo!")  # mensage de saída do laço
+
