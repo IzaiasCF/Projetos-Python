20036f2feb9350c68d3c227e545cfe8386144254
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-08 11:58:08 -0300

    add

--- push_projetos_python.bat
+++ push_projetos_python.bat
@@ -1,10 +1,8 @@
 @echo off
 :: Caminho até sua pasta local do repositório
 cd /d "C:\Users\IzaiasCF\Documents\Projetos\Projetos-Python"
-
 :: Mensagem de commit padrão (você pode editar)
 set /p msg="Digite a mensagem do commit: "
-
 :: Executa os comandos Git
 git add .
 git commit -m "%msg%"
