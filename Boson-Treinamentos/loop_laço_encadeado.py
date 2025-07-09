13035d370f16621f6b7d405df150253c561d1c45
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-09 10:26:35 -0300

    add commit

--- Boson-Treinamentos/loop_laço_encadeado.py
+++ Boson-Treinamentos/loop_laço_encadeado.py
@@ -1,3 +1,12 @@
+# exemplo 1
+# for cont_ex in range(1,6):
+#   print(f'\nRodada: {cont_ex}')  # "f" 'de f-string
+
+# exemplo 2
 for cont_ex in range(1,6):
-  print(f'\nRodada: {cont_ex}')
+  print(f"\nRodada: {cont_ex}")
+  for cont_in in range(5, 0, -1):
+    print(f"Valor: {cont_in}")
+
+print("Fim dos laços!") 
 
