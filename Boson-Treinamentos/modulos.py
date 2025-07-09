9ca6b49265d5ce1429365fbba1d551eaf23927f3
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-09 11:19:45 -0300

    add

---
+++ Boson-Treinamentos/modulos.py
@@ -0,0 +1,9 @@
+# IMPORTANDO BIBLIOTECAS
+
+# import math  # impota todo o pacote da biblioteca
+# from math import *  # importa todo o pacote da biblioteca
+# from math import sqrt  # importa somente a biblioteca sqrt
+from math import sqrt, sin  # importa as bibliotecas sqrt e seno
+
+print(sqrt(81))  # sqrt: raiz quadrada
+
