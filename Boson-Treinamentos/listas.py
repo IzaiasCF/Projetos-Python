a8039d4813e2048b15af2dbd30f2c569f292b95d
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-09 14:37:45 -0300

    add

--- Boson-Treinamentos/listas.py
+++ Boson-Treinamentos/listas.py
@@ -6,8 +6,8 @@ notas = [5, 7, 8, 6, 9]
 print(notas)
 
 # exemplo 2
-n1 = [4,6,7,8,0,3]
-n2 = [1,6,3,0,12,4]
+n1 = [4, 6, 7, 8, 0, 3]
+n2 = [1, 6, 3, 0, 12, 4]
 valores = n1 + n2
 # print(valores)
 # print(type(valores))
@@ -18,7 +18,7 @@ n2 = [1, 6, 3, 0, 12, 4]
 valores = n1 + n2
 
 print(valores[0])  # print do primeiro valor na lista
-print(valores[6])  # print do sexto valor da 
+print(valores[6])  # print do sexto valor da
 print(valores[-1])  # print do último valor
 print(valores[-2])  # print do penúltimo valor
 
@@ -28,5 +28,5 @@ print(valores[0])
 
 # exemplo 5
 print(valores[0:2])  # imprime os valores de 0 a 2, na posição
-print(valores)
+print(valores[:4])  # imprime os 04 primeros valores, na posição
 
