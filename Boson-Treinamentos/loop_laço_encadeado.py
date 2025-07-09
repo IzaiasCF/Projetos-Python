afe6930afcf898e7a894ee587038594fb4c92761
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-09 10:33:50 -0300

    add

--- Boson-Treinamentos/loop_laço_encadeado.py
+++ Boson-Treinamentos/loop_laço_encadeado.py
@@ -3,10 +3,19 @@
 #   print(f'\nRodada: {cont_ex}')  # "f" 'de f-string
 
 # exemplo 2
-for cont_ex in range(1,6):
-  print(f"\nRodada: {cont_ex}")
-  for cont_in in range(5, 0, -1):
-    print(f"Valor: {cont_in}")
+# for cont_ex in range(1,6):
+#   print(f"\nRodada: {cont_ex}")
+#   for cont_in in range(5, 0, -1):
+#     print(f"Valor: {cont_in}")
 
-print("Fim dos laços!") 
+# print("Fim dos laços!") 
+
+# import
+import random
+
+for A in range(1,6):
+  print(f"\Conjunto {A}")
+  for B in range(5):
+    num = random.randint(1,100)
+    print(f"Valor: {num}")
 
