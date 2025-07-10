eb07529310cba6f4510cc9fb3c8efc136cea2828
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-10 10:57:49 -0300

    add

--- Boson-Treinamentos/tuplas.py
+++ Boson-Treinamentos/tuplas.py
@@ -1,6 +1,6 @@
 # tuplas são IMUTÁVEIS!!!
 # tupla = (2,4,6,7,9)
-# print(tupla) 
+# print(tupla)
 
 # exemplo 1
 # halogenios = ("F","Cl","Br","I","At")
@@ -12,7 +12,9 @@
 halogenios = ("F", "Cl", "Br", "I", "At")
 gases_nobres = ("He", "Ne", "Ar", "Xe", "Kr", "Rn")
 elementos = halogenios + gases_nobres
-print(elementos)
+# print(elementos)
 
-t1 = (5,2,6,8,4,5,6,4,4,0,12,223,4,5)
-print(t1.count(5))
+# t1 = (5, 2, 6, 8, 4, 5, 6, 4, 4, 0, 12, 223, 4, 5)
+# print(t1.count(5))  # contar quantas vezes o elemento "5" aparece na lista
+
+print(halogenios[0:2])  # printar 02 elementos da lista a partir da posição zero(0) 
