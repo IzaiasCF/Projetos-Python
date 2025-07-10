754e84eba3f2a1441672faa83c1acd1e6d017b6b
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-10 11:41:26 -0300

    add

--- Boson-Treinamentos/tuplas.py
+++ Boson-Treinamentos/tuplas.py
@@ -17,7 +17,27 @@ elementos = halogenios + gases_nobres
 # t1 = (5, 2, 6, 8, 4, 5, 6, 4, 4, 0, 12, 223, 4, 5)
 # print(t1.count(5))  # contar quantas vezes o elemento "5" aparece na lista
 
-print(halogenios[0:2])  # printar 02 elementos da lista a partir da posição zero(0)
-print(halogenios[:3])  # printar elemento do começo à posição 3
-print("F" in halogenios)  # perguntar se um elemento (no caso, "F") está na lista
-print("Fe" in halogenios)  # perguntar se um elemento (no caso, "Fe") está na lista
+# print(halogenios[0:2])  # printar 02 elementos da lista a partir da posição zero(0)
+# print(halogenios[:3])  # printar elemento do começo à posição 3
+# print("F" in halogenios)  # perguntar se um elemento (no caso, "F") está na lista
+# print("Fe" in halogenios)  # perguntar se um elemento (no caso, "Fe") está na lista
+
+# exemplo 3
+t1 = (5, 2, 6, 8, 4, 5, 6, 4, 4, 0, 12, 22, 4, 5)
+# print(sum(t1))  # somar elemntos quando números
+# print(min(t1))  # mostrar menor valor
+# print(max(t1))  # mostrar maior valor
+
+# OPERAÇÕES NAO DISPONÍVEIS EM TUPLAS: .sort(), .append(), .reverse(), .pop()
+# TUPLAS SÃO IMUTÁVEIS
+
+# exemplo 4
+# for elemento in elementos:
+#    print(f"Elemento químico: {elemento}")
+
+# grupo17 = list(halogenios)
+# print(grupo17)
+
+grupo17 = list(halogenios)
+grupo17[0] = "H"
+print(grupo17)
