25e1022b7795abfd2f2b55925af00130b39eb6a8
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-10 10:24:07 -0300

    add commit

---
+++ Boson-Treinamentos/tuplas.py
@@ -0,0 +1,10 @@
+# tuplas são IMUTÁVEIS!!!
+# exemplo 1
+# tupla = (2,4,6,7,9)
+# print(tupla) 
+
+# exemplo 2
+halogenios = ("F","Cl","Br","I","At")
+print(len(halogenios))  # len: descobrir quantos elementos tem em "halogenios"
+print(halogenios[3])  # [3]: visualizar posição 3 da tupla halogenios
+print(halogenios[-1])  # [-1]: visualizar último ítem de halogenios
