3ceff811c1da57dd1b1c02ffe87f91af289a6656
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-11 11:11:10 -0300

    add lines

--- exemplos/teste.py
+++ exemplos/teste.py
@@ -2,3 +2,6 @@ from colorama import init, Fore
 init()
 
 print(Fore.GREEN + "🌈 Olá, pessoal! Meu primeiro programa em Python!" + Fore.RESET)
+print(Fore.YELLOW + "🌈 Olá, pessoal! Meu primeiro programa em Python!" + Fore.RESET)
+print(Fore.RED + "🌈 Olá, pessoal! Meu primeiro programa em Python!" + Fore.RESET)
+
