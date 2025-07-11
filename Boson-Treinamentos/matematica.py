47c13cfee8e0e6e7f70748a15f862db3831585de
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-11 10:23:28 -0300

    add lines

--- Boson-Treinamentos/matematica.py
+++ Boson-Treinamentos/matematica.py
@@ -17,3 +17,10 @@
 # módulo math
 import math
 
+x = 8
+y = 100
+raiz_quadrada = math.sqrt(x)
+print(math.ceil(raiz_quadrada))  # arrendar para o valor inteiro acima
+print(math.floor(raiz_quadrada))  # arrendar para o valor inteiro abaixo
+
+
