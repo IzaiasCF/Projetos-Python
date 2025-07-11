d25a8af157b1c278858701d880d58b115a372972
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-11 09:59:32 -0300

    add lines

---
+++ Boson-Treinamentos/matematica.py
@@ -0,0 +1,10 @@
+# funções internas
+valores = [2,5,8,-1,0,11,7,-6]
+print(max(valores))  # valor máximo da lista
+print(min(valores))  # valor mínimo a lista
+
+# valores absolutos (valores sem sinal)
+a = -5
+b = 4
+print(abs(a))
+
