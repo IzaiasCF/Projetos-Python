1cabfff50d7bded152a6d30a26423d7ff0c84046
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-11 10:17:26 -0300

    add lines

--- Boson-Treinamentos/matematica.py
+++ Boson-Treinamentos/matematica.py
@@ -1,10 +1,19 @@
 # funções internas
-valores = [2,5,8,-1,0,11,7,-6]
-print(max(valores))  # valor máximo da lista
-print(min(valores))  # valor mínimo a lista
+# valores = [2, 5, 8, -1, 0, 11, 7, -6]
+# print(max(valores))  # valor máximo da lista
+# print(min(valores))  # valor mínimo a lista
 
 # valores absolutos (valores sem sinal)
-a = -5
-b = 4
-print(abs(a))
+# a = -5
+# b = 4
+# print(abs(a))
+
+# exponenciação
+# print(pow(a, b))
+# c = 2.789011
+# print(round(c, 3))  # exponenciação com 03 casas decimais
+# print(round(c, 1))  # exponencia'ção com 01 casa decimal
+
+# módulo math
+import math
 
