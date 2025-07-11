2bff2227216c3878060dad74b2ad7832276cd5ce
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-11 11:13:22 -0300

    add

--- exemplos/variaveis.py
+++ exemplos/variaveis.py
@@ -1,24 +1,24 @@
 ## Variáveis
 
-# nome_usuario = 'Izaias'
-# print(nome_usuario)
+nome_usuario = 'Izaias'
+print(nome_usuario)
 
-# media = 0
-# n1 = n2 = n3 = n4 = 0.0
-# nome, idade = 'Izaias', 58
-# estado = True
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Izaias', 58
+estado = True
 
 # Função type()
-# print(type(media))
-# print(type(n2))
-# print(type(nome))
-# print(type(estado))
-# print(type(1+2j))
+print(type(media))
+print(type(n2))
+print(type(nome))
+print(type(estado))
+print(type(1+2j))
 
 # Função isinstance()
-# a = 10
-# b = 'sol'
-# print(isinstance(a, (int, float)))
+a = 10
+b = 'sol'
+print(isinstance(a, (int, float)))
 
 a = 40
 c = 3
