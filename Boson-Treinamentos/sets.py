cbdbdf8143af4d3e29f47aa1035201faadbb5c8c
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-12 14:49:12 -0300

    add

--- Boson-Treinamentos/sets.py
+++ Boson-Treinamentos/sets.py
@@ -3,3 +3,7 @@
 # exemplo 1
 
 planeta_anao = {"Plutão", "Ceres", "Eris", "Haumea", "Makemake",}
+print(planeta_anao)  # imprime o conteúdo de "planeta_anão"
+print(len(planeta_anao))  # imprime a quantidade de elementos de "planeta_ana"
+print("Ceres" in planeta_anao)  # IN: pergunta se "Ceres" está em "planeta_anao"
+
