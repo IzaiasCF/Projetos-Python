4dc75e229ecdfeb46b3e9d3941305bdcd4a60081
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-12 10:40:06 -0300

    add file

---
+++ Boson-Treinamentos/strings.py
@@ -0,0 +1,11 @@
+# exdmplo 1
+# nome = "Izaias"
+# letra = nome[2]  # [2] segunda letra de 'Izaias', a parir do zero
+# print(letra)  # imprime na tela
+# print(len(nome))  # (len) contagem da quantidade de elementos, ou letras, no caso
+
+# exemplo 2
+nome = 'Izaias'
+escola = 'Bóson Treinamentos'
+print(nome + ' estuda na ' + escola + '.')
+
