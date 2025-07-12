e338456b0af117a198c4b4e922bd9c316b0f4aa5
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-12 10:52:35 -0300

    add commit

--- Boson-Treinamentos/strings.py
+++ Boson-Treinamentos/strings.py
@@ -5,7 +5,18 @@
 # print(len(nome))  # (len) contagem da quantidade de elementos, ou letras, no caso
 
 # exemplo 2
-nome = 'Izaias'
-escola = 'Bóson Treinamentos'
-print(nome + ' estuda na ' + escola + '.')
+# nome = 'Izaias'
+# escola = 'Bóson Treinamentos'
+# print(nome + ' estuda na ' + escola + '.')
+
+# exemplo 3
+# email = input("Digite seu endereço de e-mail:")
+# arroba = email.find("@")  # quantidade de elementos antes de '@'
+# print(arroba)
+# usuario = email[0:arroba]  # posição do primeiro nome até 'arroba'
+# dominio = email[arroba + 1 :]  # posição após o 'arroba'
+# print(usuario)
+# print(dominio)
+
+# exemplo 4
 
