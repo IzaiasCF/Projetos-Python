e93f3bc766d05ae34b9cd1025f80169a1abd19d7
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-12 15:02:05 -0300

    add

--- Boson-Treinamentos/sets.py
+++ Boson-Treinamentos/sets.py
@@ -1,9 +1,22 @@
 # Set - conjuntos em Python
 
 # exemplo 1
-
 planeta_anao = {"Plutão", "Ceres", "Eris", "Haumea", "Makemake",}
-print(planeta_anao)  # imprime o conteúdo de "planeta_anão"
-print(len(planeta_anao))  # imprime a quantidade de elementos de "planeta_ana"
-print("Ceres" in planeta_anao)  # IN: pergunta se "Ceres" está em "planeta_anao"
+
+# print(planeta_anao)  # imprime o conteúdo de "planeta_anão"
+# print(len(planeta_anao))  # imprime a quantidade de elementos de "planeta_ana"
+# print("Ceres" in planeta_anao)  # IN: pergunta se "Ceres" está em "planeta_anao"
+
+# print("lua" in planeta_anao)
+# print("lua" not in planeta_anao)
+
+# for astro in planeta_anao:
+#     print(astro.upper(), end="")  # imprime todo o conteúdo sem mudar de linha
+#     print(astro.upper())
+
+# exemplo 2
+astros = ["Lua", "Vênus", "Sírius", "Marte", "Lua"]
+print(astros, end=" ")
+astro_set = set(astros)
+print(astro_set)
 
