4b326436eac45d79c5fbb2cfa8b1197aab812e84
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-12 13:54:00 -0300

    add

--- Boson-Treinamentos/dicionarios.py
+++ Boson-Treinamentos/dicionarios.py
@@ -1,4 +1,4 @@
-# Dicionários 
+# Dicionários
 
 # exmplo 1
 elemento = {
@@ -7,7 +7,29 @@ elemento = {
    "grupo": "Metais Alcalinos",
    "densidade": 0.534
 }
+print(elemento)
 print(f"Elemento: {elemento["nome"]}")  # imprime o que está contido em "nome"
 print(f"Densidade: {elemento["densidade"]}")  # imprime o que está contido em "densidade"
 print(f"O dicionário possui {len(elemento)} elementos.")  # imprime a quantidade de elementos de "elemento" (o Dicionário)
+
+# atualizar uma entrada
+elemento["grupo"] = "Alacalinos"
+print(elemento)
+
+# adicionar uma entrada
+elemento["período"] = 1
+print(elemento)
+
+# exclusão de ítens em dicionários
+del elemento["período"]
+print(elemento)
+
+# deletar todo o conteúdo do dicionário
+elemento.clear()
+print(elemento)
+
+# deletar todo o dicionário
+del elemento
+print(elemento)
+
 
