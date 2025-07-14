2b61bc778fa3fe4fca9e12e569d87be4a59661e4
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-14 13:31:30 -0300

    add

--- Boson-Treinamentos/funções.py
+++ Boson-Treinamentos/funções.py
@@ -1,2 +1,14 @@
 # Funções - Modularização, Reuso de código, Legibilidade
 
+# exemplo 1
+# def mensagem():
+#   print("Bóson treinamenrtos em Tecnologia")
+#   print("Curso Completo de Python")
+
+# mensagem()
+
+# exemplo 2
+def soma(a, b):
+  print(a + b)
+
+soma(12,7)
