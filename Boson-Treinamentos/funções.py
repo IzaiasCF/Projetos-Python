19fcc65859b1bfe8f75c12b480b80e86368ef3f4
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-14 15:46:20 -0300

    add

--- Boson-Treinamentos/funções.py
+++ Boson-Treinamentos/funções.py
@@ -7,6 +7,16 @@
 # mensagem()
 
 # exemplo 2
-def soma(a, b):
-  print(a + b)
-soma(12,7)
+# def soma(a, b):
+#   print(a + b)
+# soma(12,7)
+
+# exemplo 3
+def mult(x, y):
+  return x + y
+
+a = 5
+b = 8
+c = mult(a, b)
+
+  
