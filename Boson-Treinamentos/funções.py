891f46a5e34c2c6f601adad9ade7dbf58301e884
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-14 15:53:56 -0300

    add

--- Boson-Treinamentos/funções.py
+++ Boson-Treinamentos/funções.py
@@ -12,10 +12,22 @@
 # soma(12,7)
 
 # exemplo 3
-def mult(x, y):
-  return x * y
+# def mult(x, y):
+#   return x * y
 
-a = 5
-b = 8
-c = mult(a, b)  
-print(f"O produto de {a} e {b} é {c}.")
+# a = 5
+# b = 8
+# c = mult(a, b)
+# print(f"O produto de {a} e {b} é {c}.")
+
+
+# exemplo 4
+def div(k, j):
+    return k / j
+
+if __name__ == "__main__":
+    a = int(input("Digite um número: "))
+    b = int(input("Digite outro número: "))
+
+    r = div(a, b)
+    print(f"{a} dividido por {b} é igaul a {r}.")
