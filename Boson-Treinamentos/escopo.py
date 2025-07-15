e4af65aa680010ff5dbf136d4723c216f53402fd
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-15 12:03:58 -0300

    add

--- Boson-Treinamentos/escopo.py
+++ Boson-Treinamentos/escopo.py
@@ -1,13 +1,25 @@
 # Escopo global e local
 
 # exemplo 1
+# var_global = "Curso Completo de Python"
+
+# def escreve_texto():
+#   var_local = "Fabio dos Reis"   # variável dentro dsa função
+#   print(f"Variável Global: {var_global}")
+#   print(f"Variável Local: {var_local}")
+
+# if __name__ == "__main__":
+#     print(f"Executando a função escreve_texto")
+#     escreve_texto()
+
 var_global = "Curso Completo de Python"
+var_local = "Fabio dos Reis"   # variával fora da função
 
 def escreve_texto():
-  var_local = "Fabio dos Reis"
-  print(f"Variável Global: {var_global}")
-  print(f"Variável Local: {var_local}")
+    print(f"Variável Global: {var_global}")
+    print(f"Variável Local: {var_local}")
 
 if __name__ == "__main__":
-  print(f"Executando a função escreve_texto")
-  escreve_texto()
+    print(f"Executando a função escreve_texto")
+    escreve_texto()
+
