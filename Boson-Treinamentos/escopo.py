5e25934ade434df60d4fed845abbe6ee2d8f3c21
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-17 10:52:11 -0300

    add

--- Boson-Treinamentos/escopo.py
+++ Boson-Treinamentos/escopo.py
@@ -23,10 +23,31 @@
 #     print(f"Executando a função escreve_texto")
 #     escreve_texto()
 
+
+# exemplo 2
+# var_global = "Curso Completo de Python"
+# var_local = "Fabio dos Reis"  # VARIÁVEL FORA DA FUNÇÃO
+
+# def escreve_texto():
+#     print(f"Variável Global: {var_global}")
+#     print(f"Variável Local: {var_local}")
+
+# if __name__ == "__main__":
+#     print(f"Executando a função escreve_texto")
+#     escreve_texto()
+
+#     print("Tentando aessar as variáveis diretamente: ")
+#     print(f"Variável Global: {var_global}")
+#     print(f"Variável Local: {var_local}")   # NÀO seria imprimido na tela, se a variável 'var_local' estivesse dentro da função
+
+
+# exemplo 3
 var_global = "Curso Completo de Python"
 var_local = "Fabio dos Reis"  # VARIÁVEL FORA DA FUNÇÃO
 
 def escreve_texto():
+    #global var_global
+
     print(f"Variável Global: {var_global}")
     print(f"Variável Local: {var_local}")
 
@@ -34,6 +55,6 @@ if __name__ == "__main__":
     print(f"Executando a função escreve_texto")
     escreve_texto()
 
-    print("Tentando aessar as variáveis diretamente: ")
+    print("\nTentando aessar as variáveis diretamente: ")
     print(f"Variável Global: {var_global}")
-    print(f"Variável Local: {var_local}")   # NÀO seria imprimido na tela, se a variável 'var_local' estivesse dentro da função
+    print(f"Variável Local: {var_local}")
