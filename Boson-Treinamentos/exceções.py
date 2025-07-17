2ad78c9245db51d30c422b20555d54029568e1bc
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-17 11:29:52 -0300

    add

--- Boson-Treinamentos/exceções.py
+++ Boson-Treinamentos/exceções.py
@@ -1,18 +1,19 @@
 # Exceção é um objeto que representa um erro que ocorreu ao executar um programa
 # Blocos try ... except
 
-# EXEMPLO 1
+# EXEMPLO 1: se dividir por zero, vai dar erro!
 # n1 = int(input("Digite um número: "))
-# n2 = int(input("Digite outro número: "))  # se dividir por zero, vai dar erro!
+# n2 = int(input("Digite outro número: "))
 
 # r = round(n1 / n2, 2)
 
 # print(f"Resulado: {r}")
 
+
+# EXEMPLO 2: tratando exceções de erros com try .. except
 n1 = int(input("Digite um número: "))
 n2 = int(input("Digite outro número: "))
 
-# tratando exceções de erros com try .. except
 try:
     r = round(n1 / n2, 2)
 except ZeroDivisionError:
