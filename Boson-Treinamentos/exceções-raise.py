c30acfc4f38d1860f929276b861d2a78dd8bc806
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-18 09:43:55 -0300

    add

--- Boson-Treinamentos/exceções-raise.py
+++ Boson-Treinamentos/exceções-raise.py
@@ -1,14 +1,34 @@
 # Tratando EXCEÇÕES com RAISE
 
 # EXEMPLO 1
+# from math import sqrt
+
+# if __name__ == "__main__":
+#     try:
+#         num = int(input("Digite um numero positivo: "))
+#         if num < 0:
+#             raise ArithmeticError
+#     except ArithmeticError:
+#         print(f"ERROR: Foi digitado um número negativo.")
+#     else:
+#         print(f"A raiz quadrada de {num} é {sqrt(num)}")
+#     finally:
+#         print(f"\n>>> FIM DO CÁLCULO <<<")
+
+
+# EXEMPLO 2
 from math import sqrt
+
+class NumeroNegativoError(Exception):
+    def __init__(self):
+        pass
 
 if __name__ == "__main__":
     try:
         num = int(input("Digite um numero positivo: "))
         if num < 0:
-            raise ArithmeticError
-    except ArithmeticError:
+            raise NumeroNegativoError
+    except NumeroNegativoError:
         print(f"ERROR: Foi digitado um número negativo.")
     else:
         print(f"A raiz quadrada de {num} é {sqrt(num)}")
