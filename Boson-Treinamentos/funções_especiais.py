8632209c1ceea44b5496764e38b3852518bbcf8a
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-18 12:07:45 -0300

    add

---
+++ Boson-Treinamentos/funções_especiais.py
@@ -0,0 +1,15 @@
+# FUNÇÕES LAMBDA (anônimas)
+#   Sintaxe:
+#   lambda argumentos: expressão
+
+# EXEMPLO 1
+# quadrado = lambda x: x**2
+
+# for i in range(1, 11):
+#   print(quadrado(i))
+
+
+# EXEMPLO 2  # verificação se número e par
+par = lambda x : x %2 == 0
+print(par(8))
+print(par(9))
