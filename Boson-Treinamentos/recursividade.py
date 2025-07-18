11fa6a8c78f50a12b25f8e53112d0f5bdba34f97
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-18 10:48:48 -0300

    add

--- Boson-Treinamentos/recursividade.py
+++ Boson-Treinamentos/recursividade.py
@@ -7,3 +7,14 @@
 
 
 # EXEMPLO 1
+def fatorial(numero):
+  if numero == 0 or numero == 1:
+    return 1
+  else:
+    return numero * fatorial(numero - 1)
+  
+if __name__ == "__main__":
+  x = int(input("Digite um número inteiro positivo para calcular seu fatorial: "))
+  res = fatorial(x)
+  print(f"O fatorial de {x} é {res}")
+  
