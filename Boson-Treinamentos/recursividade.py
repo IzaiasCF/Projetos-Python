cf411d72d7fd2b8e738f603a0f4042ccb3eedbcd
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-18 10:20:05 -0300

    add file

---
+++ Boson-Treinamentos/recursividade.py
@@ -0,0 +1,9 @@
+# RECURSIVIDADE
+# FÓRMULA GERAL PARA O FATORIAL:
+# fatorial(num) = 1, se num = 0 ou se num = 1  -- caso base
+# OU: fatorial(num) = num * fatorial(num -1), para num > 1  -- caso recursivo
+# exemplo: 4! -> 4 * fatorial(3) = 4 * 3 * fatorial(2) = 4 * 3 * 2 * 1
+# 4! = 4 * 3 * 2 * 1 = 24  (total)
+
+
+# EXEMPLO 1
