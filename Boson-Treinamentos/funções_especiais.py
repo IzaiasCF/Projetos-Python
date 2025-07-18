c56533d838fd389f2ef4fc6ce5d13dedbbd3021b
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-18 13:14:21 -0300

    add

--- Boson-Treinamentos/funções_especiais.py
+++ Boson-Treinamentos/funções_especiais.py
@@ -33,9 +33,17 @@
 
 
 # EXEMPLO 6
-def numeros_pares(n):
-  return n % 2 == 0
+# def numeros_pares(n):
+#   return n % 2 == 0
 
-numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-num_par = list(filter(numeros_pares, numeros))
-print(num_par)
+# numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+# num_par = list(filter(numeros_pares, numeros))
+# print(num_par)
+
+
+# EXEMPLO 7
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+num_impar = list(filter(lambda x: x % 2 != 0, numeros))
+print(numeros)
+print(num_impar)
+
