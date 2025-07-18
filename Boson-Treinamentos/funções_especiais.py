4417aa35e341048101e089d01286203a35fb0e45
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-18 14:50:01 -0300

    add

--- Boson-Treinamentos/funções_especiais.py
+++ Boson-Treinamentos/funções_especiais.py
@@ -65,7 +65,7 @@
 from functools import reduce
 
 numeros = [1, 2, 3, 4]
-# intenção: ((1² + 2²)² + 3²)² + 4²
 
+# intenção: ((1² + 2²)² + 3²)² + 4²
 total = reduce(lambda x, y: x**2 + y**2, numeros)
 print(total)
