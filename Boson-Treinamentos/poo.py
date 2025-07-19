10db3fa809ea19c287cc347eb0079d2b6a3440f1
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-19 11:09:06 -0300

    add

--- Boson-Treinamentos/poo.py
+++ Boson-Treinamentos/poo.py
@@ -12,16 +12,24 @@ class Veiculo:
         self.__modelo = modelo
         self.__num_registro = None
 
+
 # SETTER
 def set_num_registro(self, registro):
-  self.__num_registro = registro
+    self.__num_registro = registro
+
 
 # GETTER
 def get_fabr_modelo(self):
     print(f"Modelo: {self.__modelo} - Fabricante: {self.__fabricante}.\n")
 
 
+def get_num_reistro(self):
+    return self.__num_registro
+
+
 if __name__ == "__main__":
     meu_veiculo = Veiculo("GM", "Cadillac Escalade")
     meu_veiculo.movimentar()
     meu_veiculo.get_fabr_modelo()
+    meu_veiculo.set_num_registro(490321 - 1)
+    print(f"Registro: {meu_veiculo.get_num_registro()}")
