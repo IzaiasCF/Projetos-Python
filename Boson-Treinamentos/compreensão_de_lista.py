5c97b4b2490c68743c3a71d82506ed15dbd1a4c2
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-19 09:36:37 -0300

    add

--- Boson-Treinamentos/compreensão_de_lista.py
+++ Boson-Treinamentos/compreensão_de_lista.py
@@ -12,5 +12,14 @@
 
 
 # EXEMPLO 3  -  CRIAR LISTA DE PARES DE 0 A 10
-pares = [num for num in range(120) if num % 2 == 0]
-print(pares)
+# pares = [num for num in range(120) if num % 2 == 0]
+# print(pares)
+
+
+# EXEMPLO 4  -  DESCOBRINDO UMA OLISTA DE VOGAIS
+frase = "A lógica e apenas o princípio da sabedoria, não o seu fim."
+vogais = ["A", "a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú"]
+
+lista_vogais = [v for v in frase if v in vogais]
+print(f"A frase possui {len(lista_vogais)} vogais.")
+print(lista_vogais)
