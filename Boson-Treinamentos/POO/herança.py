722c406d00a79e2a9d4a04fe6f51aeb20f78b6a3
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-19 11:44:00 -0300

    add file

---
+++ Boson-Treinamentos/POO/herança.py
@@ -0,0 +1,32 @@
+# POO: PROGRAMAÇÃO ORIENTADA A OBJETOS  -  É UM PARADIGMA DE PROGRAMAÇÃO
+# HERANÇA
+
+
+# EXEMPLO 1
+class Veiculo:
+    def __init__(self, fabricante, modelo):
+        self.__fabricante = fabricante
+        self.__modelo = modelo
+        self.__num_registro = None
+
+    def movimentar(self):
+        print("Sou um veículo e me desloco!")
+
+    # SETTER
+    def set_num_registro(self, registro):
+        self.__num_registro = registro
+
+    # GETTER
+    def get_fabr_modelo(self):
+        print(f"Modelo: {self.__modelo} - Fabricante: {self.__fabricante}.\n")
+
+    def get_num_registro(self):
+        return self.__num_registro
+
+
+if __name__ == "__main__":
+    meu_veiculo = Veiculo("GM", "Cadillac Escalade")
+    meu_veiculo.movimentar()
+    meu_veiculo.get_fabr_modelo()
+    meu_veiculo.set_num_registro("490321-1")
+    print(f"Registro: {meu_veiculo.get_num_registro()}\n")
