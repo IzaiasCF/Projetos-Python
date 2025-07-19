1b41a34499e9f39c0c830e56ff139e2569e8ef5e
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-19 10:52:37 -0300

    add

--- Boson-Treinamentos/poo.py
+++ Boson-Treinamentos/poo.py
@@ -1,23 +1,24 @@
 # POO: PROGRAMAÇÃO ORIENTADA A OBJETOS  -  É UM PARADIGMA DE PROGRAMAÇÃO
 # Classes e Objetos
 
+
 # EXEMPLO 1
 class Veiculo:
-  def movimentar(self):
-    print("Sou um veículo e me desloco!")
+    def movimentar(self):
+        print("Sou um veículo e me desloco!")
 
-  def __init__(self, fabricante, modelo):
-    self.__fabricante = fabricante
-    self.__modelo = modelo
-    self.__num_registro = None
-    
+    def __init__(self, fabricante, modelo):
+        self.__fabricante = fabricante
+        self.__modelo = modelo
+        self.__num_registro = None
+
+
 # GETTER
 def get_fabr_modelo(self):
-  print(f"Modelo: {self.__modelo} - Fabricante: {self.__fabricante}.\n")
+    print(f"Modelo: {self.__modelo} - Fabricante: {self.__fabricante}.\n")
+
 
 if __name__ == "__main__":
-  meu_veiculo = Veiculo("GM", "Cadillac Escalade")
-  meu_veiculo.movimentar()
-  meu_veiculo.get_fabr_modelo()
-
-
+    meu_veiculo = Veiculo("GM", "Cadillac Escalade")
+    meu_veiculo.movimentar()
+    meu_veiculo.get_fabr_modelo()
