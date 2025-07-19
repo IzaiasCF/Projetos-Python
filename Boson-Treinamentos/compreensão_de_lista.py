5ca15f633e2cbeca2b0b3258f4fd01e48dc52a13
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-19 08:51:06 -0300

    add

--- Boson-Treinamentos/compreensão_de_lista.py
+++ Boson-Treinamentos/compreensão_de_lista.py
@@ -1,6 +1,11 @@
-# EXEMPLO 1
-numeros = [1,4,7,9,10,12,21]
+# EXEMPLO 1  -  cria-se listas sempre com colchetes
+# numeros = [1,4,7,9,10,12,21]
 
-quadrados = list(map(lambda num: num**2, numeros))
+# quadrados = list(map(lambda num: num**2, numeros))
+# print(quadrados)
+
+# EXEMPLO 2 - MELHOR MÉTODO ABAIXO:
+numeros = [1, 4, 7, 9, 10, 12, 21]
+
+quadrados = [num**2 for num in numeros]
 print(quadrados)
-
