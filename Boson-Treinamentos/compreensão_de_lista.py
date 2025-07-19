f45bec8743505ec74cca9fc6a70061fd19eb4f61
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-19 09:00:33 -0300

    add

--- Boson-Treinamentos/compreensão_de_lista.py
+++ Boson-Treinamentos/compreensão_de_lista.py
@@ -1,11 +1,14 @@
-# EXEMPLO 1  -  cria-se listas sempre com colchetes
+# EXEMPLO 1  -  CRIA-SE LISTAS COM COLCHETES
 # numeros = [1,4,7,9,10,12,21]
 
 # quadrados = list(map(lambda num: num**2, numeros))
 # print(quadrados)
 
 # EXEMPLO 2 - MELHOR MÉTODO ABAIXO:
-numeros = [1, 4, 7, 9, 10, 12, 21]
+# numeros = [1, 4, 7, 9, 10, 12, 21]
 
-quadrados = [num**2 for num in numeros]
-print(quadrados)
+# quadrados = [num**2 for num in numeros]
+# print(quadrados)
+
+
+# EXEMPLO 3  -  CRIAR LISTA DE PARES DE 0 A 
