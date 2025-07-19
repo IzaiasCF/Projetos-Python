c128199d024454a6c2746c2ec8de2043d71dd16f
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-19 10:06:21 -0300

    add

--- Boson-Treinamentos/poo.py
+++ Boson-Treinamentos/poo.py
@@ -2,3 +2,18 @@
 # Classes e Objetos
 
 # EXEMPLO 1
+class Veiculo:
+  def movimentar(self):
+    print("Sou um veículo e me desloco!")
+
+  def __init__(self, fabricante, modelo):
+    self.fabricante = fabricante
+    self.modelo = modelo
+    self.num_registro = None
+
+if __name__ == "__main__":
+  meu_veiculo = Veiculo("GM", "Cadillac Escalade")
+  meu_veiculo.movimentar()
+  print(meu_veiculo.fabricante)
+  print(meu_veiculo.modelo)
+
