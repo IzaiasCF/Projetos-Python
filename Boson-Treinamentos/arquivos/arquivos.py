ff8e79e47854c704d42432b52d99a27ea855894d
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-23 15:57:38 -0300

    add

--- Boson-Treinamentos/arquivos/arquivos.py
+++ Boson-Treinamentos/arquivos/arquivos.py
@@ -1,5 +1,9 @@
 # Manipulção de arqivos de texto
 
-manipulador = open("C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-treinamentos\\arquivos\\arquivo.txt", "r", encoding="latin1")
+manipulador = open(
+    "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-treinamentos\\arquivos\\arquivo.txt",
+    "r",
+    encoding="latin1",
+)
 print(f"Método read():\n")
 print(manipulador.read())
