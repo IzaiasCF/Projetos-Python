50e7f38bce1785db7ae87de320745404f52c8b7b
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-25 20:18:46 -0300

    add

--- Boson-Treinamentos/arquivos/arquivos.py
+++ Boson-Treinamentos/arquivos/arquivos.py
@@ -3,6 +3,13 @@
 manipulador = open(
     "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-treinamentos\\arquivos\\arquivo.txt",
     "r",
-    encoding="utf-8")
-print(f"Método read():\n")
-print(manipulador.read())  # read: leitura
+    encoding="latin1")
+
+# print(f"Método read():\n")
+# print(manipulador.read())  # read: leitura
+
+# print(f"Método readline():\n")
+# print(manipulador.readline())  # read: leitura em linha
+
+print(f"Método readlines():\n")
+print(manipulador.readlines())  # read: leitura dentro de lista
