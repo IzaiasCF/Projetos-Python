cf6dabf88bfdf36915e5413a1bbbbea3173f0d45
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-26 09:21:09 -0300

    add

--- Boson-Treinamentos/arquivos/arquivos.py
+++ Boson-Treinamentos/arquivos/arquivos.py
@@ -18,39 +18,16 @@
 
 
 # EXEMPLO 2
-# try:
-#     manipulador = open(
-#         "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt",
-#         "r",
-#         encoding="windows-1252")
-#     for linha in manipulador:
-#         print(linha)
-
-# except IOError:
-#     print(f"Não foi possível abrir o arquivo.")
-# else:
-#     manipulador.close()
-
-
-# EXEMPLO 3
-import chardet
-
-# Caminho do arquivo
-caminho_arquivo = "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt"
-
-# Primeiro: detectar a codificação
-with open(caminho_arquivo, "rb") as f:
-    resultado = chardet.detect(f.read())
-    encoding_detectado = resultado["encoding"]
-    print(f"Codificação detectada: {encoding_detectado}")
-
-# Segundo: abrir o arquivo com a codificação detectada
 try:
-    with open(caminho_arquivo, "r", encoding=encoding_detectado) as manipulador:
-        for linha in manipulador:
-            print(linha)
-except IOError:
-    print("Não foi possível abrir o arquivo.")
+    manipulador = open(
+        "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt",
+        "r",
+        encoding="latin1")
+    for linha in manipulador:
+        print(linha)
 
+except IOError:
+    print(f"Não foi possível abrir o arquivo.")
 else:
     manipulador.close()
+
