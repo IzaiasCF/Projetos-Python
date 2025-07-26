4f7a9cbeef3d50c76be0bf6c82fe206e2e429b36
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-26 09:18:05 -0300

    add

--- Boson-Treinamentos/arquivos/arquivos.py
+++ Boson-Treinamentos/arquivos/arquivos.py
@@ -3,7 +3,7 @@
 
 # EXEMPLO 1
 # manipulador = open(
-#     "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-treinamentos\\arquivos\\arquivo.txt",
+#     "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt",
 #     "r",
 #     encoding="latin1")
 
@@ -13,20 +13,44 @@
 # print(f"\nMétodo readline():\n")
 # print(manipulador.readline())  # read: leitura em linha
 
-print(f"\nMétodo readlines():\n")
-print(manipulador.readlines())  # read: leitura do conteúdo dentro de uma lista
+# print(f"\nMétodo readlines():\n")
+# print(manipulador.readlines())  # read: leitura do conteúdo dentro de uma lista
 
 
 # EXEMPLO 2
-try:
-    manipulador = open("C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-treinamentos\\arquivos\\arquivo.txt",
-                  "r", 
-                  encoding="latin1")
-    for linha in manipulador:
-        print(linha)
+# try:
+#     manipulador = open(
+#         "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt",
+#         "r",
+#         encoding="windows-1252")
+#     for linha in manipulador:
+#         print(linha)
 
+# except IOError:
+#     print(f"Não foi possível abrir o arquivo.")
+# else:
+#     manipulador.close()
+
+
+# EXEMPLO 3
+import chardet
+
+# Caminho do arquivo
+caminho_arquivo = "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt"
+
+# Primeiro: detectar a codificação
+with open(caminho_arquivo, "rb") as f:
+    resultado = chardet.detect(f.read())
+    encoding_detectado = resultado["encoding"]
+    print(f"Codificação detectada: {encoding_detectado}")
+
+# Segundo: abrir o arquivo com a codificação detectada
+try:
+    with open(caminho_arquivo, "r", encoding=encoding_detectado) as manipulador:
+        for linha in manipulador:
+            print(linha)
 except IOError:
-    print(f"Não foi possível abrir o arquivo.")
+    print("Não foi possível abrir o arquivo.")
+
 else:
     manipulador.close()
-
