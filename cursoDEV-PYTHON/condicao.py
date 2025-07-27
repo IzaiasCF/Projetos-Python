45d9bb6be19b65de024174671d51c449899463ac
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-27 10:20:50 -0300

    add

--- cursoDEV-PYTHON/condicao.py
+++ cursoDEV-PYTHON/condicao.py
@@ -20,15 +20,13 @@
 #     print(lucro)
 
 # condição com listas
-# produtos = ["iphone", "ipad", "airpod"]
-# novo_produto = input("Digite aqui o novo produto: ")
-# novo_produto = novo_produto.lower()  # vai inserir na lista um novo produto, independente se digitarem em maiúsculo ou minúsculo
+produtos = ["iphone", "ipad", "airpod"]
+novo_produto = input("Digite aqui o novo produto: ")
+novo_produto = novo_produto.lower()  # vai inserir na lista um novo produto, independente se digitarem em maiúsculo ou minúsculo
 
-# if novo_produto in produtos:
-#     print("Produto já cadastrado")
-# else:
-#     print("Produto cadastrado com sucesso!")
-#     produtos.append(novo_produto)  # acrescenta o novo produto à lista produtos
-
-# print(produtos)
-
+if novo_produto in produtos:
+    print("Produto já cadastrado")
+else:
+    print("Produto cadastrado com sucesso!")
+    produtos.append(novo_produto)  # acrescenta o novo produto à lista produtos
+print(produtos)
