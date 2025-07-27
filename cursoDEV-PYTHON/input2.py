4662f857c9d8186ce8bacacfed535a65e0c17725
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-27 10:05:28 -0300

    add

--- cursoDEV-PYTHON/input2.py
+++ cursoDEV-PYTHON/input2.py
@@ -6,12 +6,16 @@
 # bonus1 = vendas1 * 0.01
 # print(bonus1)
 
-# # definindo o float noo input
+# definindo o float noo input
 # vendas2 = float(input("Digite suas vendas do dia: "))
 # bonus2 = vendas2 * 0.01
 # print(bonus2)
+vendas3 = float(input("Digite o valor de suas vendas do dia: "))
+bonus3 = vendas3 * 0.01
+print(vendas3)
+print(bonus3)
 
 ## Atenção ao concatenar variáveis
-vendas_dia1 = input("Vendas do dia 1: ")
-vendas_dia2 = input("Vendas do dia 2: ")
-print(f"Total das Vendas: R$ {float(vendas_dia1) + float(vendas_dia2)}")
+# vendas_dia1 = input("Vendas do dia 1: ")
+# vendas_dia2 = input("Vendas do dia 2: ")
+# print(f"Total das Vendas: R$ {float(vendas_dia1) + float(vendas_dia2)}")
