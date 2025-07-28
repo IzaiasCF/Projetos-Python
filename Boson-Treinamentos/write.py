f4539c0096841b3d4a2f252fc4b14b2607dd8a8a
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-28 15:58:13 -0300

    add

--- Boson-Treinamentos/write.py
+++ Boson-Treinamentos/write.py
@@ -1,35 +1,34 @@
 # Escrever em arquivos de texto
 
 # EXEMPLO 1
-# try:
-#     manipulador = open(
-#         "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt",
-#         "w",  # "w": escrita
-#         encoding="latin1"
-#     )
-#     manipulador.write("Bóson Treinamentos\n")
-#     manipulador.write("Como criar um arquivo de texto com Python.")
-
-# except IOError:
-#     print(f"Não foi possível abrir o arquivo.")
-# else:
-#     manipulador.close()
-
-
-# EXEMPLO 2
 try:
     manipulador = open(
         "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt",
-        "a",  # "a": append - acrescentar
+        "w",  # "w": escrita
         encoding="latin1"
     )
     manipulador.write("Bóson Treinamentos\n")
-    manipulador.write("Como criar um arquivo de texto com Python.\n")
-    manipulador.write("Nada mais a acrescentar!")
+    manipulador.write("Como criar um arquivo de texto com Python.")
 
 except IOError:
     print(f"Não foi possível abrir o arquivo.")
 else:
     manipulador.close()
 
-print(manipulador)
+
+# EXEMPLO 2
+# try:
+#     manipulador = open(
+#         "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt",
+#         "a",  # "a": append - acrescentar
+#         encoding="latin1"
+#     )
+#     manipulador.write("Bóson Treinamentos\n")
+#     manipulador.write("Como criar um arquivo de texto com Python.\n")
+
+# except IOError:
+#     print(f"Não foi possível abrir o arquivo.")
+# else:
+#     manipulador.close()
+
+# print(manipulador)
