45ab65bcb4c172ef3f0369bed96be1c2abec5048
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-28 16:06:31 -0300

    add

--- Boson-Treinamentos/write.py
+++ Boson-Treinamentos/write.py
@@ -32,3 +32,22 @@ else:
     manipulador.close()
 
 print(manipulador)
+
+
+# EXEMPLO 3
+
+try:
+    manipulador = open(
+        "C:\\Users\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\arquivos\\arquivo.txt",
+        "a",  # "a": append - acrescentar
+        encoding="latin1",
+    )
+    manipulador.write("\nPython é muito empregado em I.A.\n")
+    manipulador.write("A Inteligência Artificial veio para ficar!")
+
+except IOError:
+    print(f"Não foi possível abrir o arquivo.")
+else:
+    manipulador.close()
+
+print(manipulador)
