cdea9302042756f73d1b1cd5133beeff2f079216
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-29 11:15:05 -0300

    add

--- Boson-Treinamentos/tkinter/tkinter.py
+++ Boson-Treinamentos/tkinter/tkinter.py
@@ -1,5 +1,14 @@
+# INTERFACE GRÁFICA COM TKINTER
+
 import tkinter as tk
 
 janela = tk.Tk()
 janela.title("Primeiro APP")
 janela.geometry("300x100+20+20")
+
+# criar e posicionar um label com uma mensagem
+iblMsg = tk.Label(janela, text="Olá, pessoal!")
+iblMsg.pack()
+
+# exibir a janela
+janela.mainloop()
