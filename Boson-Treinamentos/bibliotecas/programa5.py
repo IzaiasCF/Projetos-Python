815666e809c205025a27ed9ffab03fb34c5950a5
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-30 11:27:26 -0300

    add file

--- Boson-Treinamentos/bibliotecas/programa5.py
+++ Boson-Treinamentos/bibliotecas/programa5.py
@@ -7,3 +7,26 @@ janela = tk.Tk()
 janela.geometry("300x200")
 janela.title("Uso de Labels")
 
+# inserindo ícone
+janela.iconbitmap(
+    "C:\\Users\\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\bibliotecas\\favicon.ico"
+)
+
+# primeiro label
+label1 = ttk.Label(
+    janela, 
+    text = "Texto do Label 1",
+    font=("helvetica, 18")
+)
+label1.pack(ipadx=10, ipady=30)
+
+# segundo label
+label2 = ttk.Label(
+    janela,
+    text = "Texto do Label 2",
+    font=("Arial", 20),
+    foreground="blue"
+)
+label2.pack(ipadx=10, ipady=60)
+
+janela.mainloop()
