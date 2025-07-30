e30b1c92121cba02a4c12449fc5832825592fafe
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-30 10:38:55 -0300

    add

--- Boson-Treinamentos/bibliotecas/programa3.py
+++ Boson-Treinamentos/bibliotecas/programa3.py
@@ -2,6 +2,7 @@
 
 import tkinter as tk
 
+# criando seguna janela
 def abrir_segunda_janela():
     segunda_janela = tk.Toplevel()
     segunda_janela.title("Segunda Janela")
@@ -19,5 +20,13 @@ def abrir_segunda_janela():
     x = (largura_tela - largura_janela) // 2
     y = (altura_tela - altura_janela) // 2
 
-    
+    # definindo a geometria da janela 2
+    segunda_janela.geometry(f"{largura_janela}x{altura_janela}+{x}+{y}")
+
+# criando janela principal
+janela_principal = tk.Tk()
+janela_principal.title("Janela principal")
+janela_principal.geometry("600x500")
+
+janela_principal.bind("<Buttom-1>", lambda event: abrir_segunda_janela())  ### (Buttom-1) botão esquerdo do mouse
 
