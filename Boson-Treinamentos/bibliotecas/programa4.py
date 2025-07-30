74a8d5385c3b6ee280a2d86acea408a8c0973755
IzaiasCF <izaiascorreafilho@gmail.com>
2025-07-30 11:12:59 -0300

    add

--- Boson-Treinamentos/bibliotecas/programa4.py
+++ Boson-Treinamentos/bibliotecas/programa4.py
@@ -7,6 +7,11 @@ janela = tk.Tk()
 janela.geometry("300x200")
 janela.title("Uso de Labels")
 
+# inserindo ícones
+janela.iconbitmap(
+    "C:\\Users\\\IzaiasCF\\Documents\\Projetos\\Projetos-Python\\Boson-Treinamentos\\bibliotecas\\favicon.ico"
+)
+
 # criando o label
 label = tk.Label(janela, text="Aula de labels")
 
