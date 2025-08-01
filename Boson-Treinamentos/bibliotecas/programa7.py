dd77f79d1111e702eaa2310b176ba8190f14be2c
IzaiasCF <izaiascorreafilho@gmail.com>
2025-08-01 09:27:02 -0300

    atualizar

--- Boson-Treinamentos/bibliotecas/programa7.py
+++ Boson-Treinamentos/bibliotecas/programa7.py
@@ -1,2 +1,19 @@
 # botões com tkinter
+# widget button
 
+import tkinter as tk
+from datetime import date
+
+def mostrar_date():
+    hoje = date.today()
+    texto_data.set(hoje.strftime("%d/%m/%y"))
+
+# criar a janela principal
+janela = tk.Tk()
+janela.title("Mostrar a Data")
+janela.geometry("300x200")
+
+# variável para armazenar o texto do label
+texto_data = tk.StringVar()
+
+# label
