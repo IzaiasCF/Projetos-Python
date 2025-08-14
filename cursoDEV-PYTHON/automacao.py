9f728310703b55f3641adcddfcea7bd41bc62ea4
IzaiasCF <izaiascorreafilho@gmail.com>
2025-08-14 11:00:32 -0300

    add refresh

--- cursoDEV-PYTHON/automacao.py
+++ cursoDEV-PYTHON/automacao.py
@@ -6,6 +6,13 @@ import time
 # criando função
 def tarefa():
     print("Rodando Automação")
+    time.sleep(2)
+    # abrir navegador
+    pyautogui.press("win")
+    time.sleep(2)
+    pyautogui.write("chrome")
+    pyautogui.press("enter")
+    # entrar no Teams
 
 
 # associar função a uma combinação de teclas
