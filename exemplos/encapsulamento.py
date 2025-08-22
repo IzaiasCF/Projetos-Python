d2a28639ee264924dafb03e7f5f173cc911e2e36
IzaiasCF <izaiascorreafilho@gmail.com>
2025-08-22 10:04:54 -0300

    add config

--- exemplos/encapsulamento.py
+++ exemplos/encapsulamento.py
@@ -1,11 +1,11 @@
 class Pessoa:
     def __init__(self, nome, cpf):
         self.__nome = nome  # Atributo privado
-        self.__cpf = cpf    # Atributo privado
+        self.__cpf = cpf  # Atributo privado
 
     # Métodos para acessar o nome
     def get_nome(self):
-        return self.__nome   # "__": encapsulamento para proteger dados sensíveis
+        return self.__nome  # "__": encapsulamento para proteger dados sensíveis
 
     def set_nome(self, novo_nome):
         self.__nome = novo_nome
@@ -14,8 +14,9 @@ class Pessoa:
     def get_cpf(self):
         return self.__cpf  # "__": encapsulamento para proteger dados sensíveis
 
+
 # Criando uma pessoa
-p = Pessoa("João", "123.456.789-00")
+p = Pessoa("Izaias", "090.588.448-54")
 
 # Acessando os dados de forma segura
 print("Nome:", p.get_nome())
