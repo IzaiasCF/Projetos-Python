3a767380c69b1be778b27024eda522109332a6a4
IzaiasCF <izaiascorreafilho@gamil.com>
2025-10-02 12:57:25 -0300

    add: script laranjas.py

---
+++ exemplos/laranjas.py
@@ -0,0 +1,12 @@
+# Algoritmo para fazer suco de laranja
+
+laranjas = int(input("Quantas laranjas você tem? "))
+
+if laranjas >= 3:
+    print("Cortando as laranjas...")
+    print("Espremendo as laranjas no copo...")
+    print("Adicionando açúcar a gosto...")
+    print("Misturando...")
+    print("✅ Suco pronto!")
+else:
+    print("⚠️ Laranjas insuficientes para fazer o suco!")
